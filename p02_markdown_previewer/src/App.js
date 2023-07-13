5aaac72d213974bea4c2c5527eed43340efda64b
Ahmet <ahmetoksuz@surgedu.com>
2023-07-13 21:47:52 +0200

    markdown previewer

--- p02_markdown_previewer/src/App.js
+++ p02_markdown_previewer/src/App.js
@@ -4,23 +4,59 @@ import {marked} from 'marked'
 
 
 function App() {
-  const [previewValue, setPreviewValue] = useState('# Welcome to my React Markdown Previewer!\n  ## a sub heading element (H2 size)\n hier is a [link](https://surgegifting.com) \n\n  this is an inline code: `<a href="https://surgegifting.com/"></a>` \n\n  and a code block: \n\n```\n\n<h1>a heading element (H1 size)</h1>\n\n<h2>a sub heading element (H2 size) </h2>\n\n<a href="https://surgegifting.com/"></a>\n\n ```\n\n An ordered list:\n\n 1. ordered\n\n 2. list\n\n 3. items\n\n An unordered list:\n\n * UNordered\n\n * list\n\n * items \n\n a blockquote:\n\n > |When you believe it something is correct;\n\n > |do it!\n\n, A/n **bolded**, _italic_, ~cross out~ text. \n\n  ![wallet](https://surgegifting.com/wp-content/uploads/2021/02/IMG_4071-scaled.jpeg)')
   
+  const initials = 
+  `
+  # Welcome to my React Markdown Previewer!
+  ## a sub heading element (H2 size)
+  ### hier is a
+   [link](https://surgegifting.com)
+  ### this is an
+   inline code: \`<a href="https://surgegifting.com/"></a>\`
+  ### and a code block: 
+  \`\`\` 
+  <h1>a heading element (H1 size)</h1>
+   <h2>a sub heading element (H2 size) </h2>
+   <a href="https://surgegifting.com/"></a>
+   \`\`\`
+  ### An ordered list:
+  1. ordered
+  2. list
+  3. items
+  
+  
+  ### An unordered list:
+  * UNordered
+  * list
+  * items
+  
+  ### a blockquote:
+   > |When you believe it something is correct;
+   > |do it!
+  
+   ###  A/n 
+   **bolded**, _italic_, ~cross out~ text. 
+  
+   ![wallet](https://surgegifting.com/wp-content/uploads/2021/02/IMG_4071-scaled.jpeg)
+  `;
+
+  const [previewValue, setPreviewValue] = useState(initials)
+
   const updatePreview=function(e){
     setPreviewValue(e.target.value);
   }
-/*
-  const renderer = new marked.Renderer();
 
-renderer.paragraph = function (text) {
-  return text.replace(/\r?\n/g, '<br>');
-};
-const options = {
-  renderer: renderer
-};
-*/
+//   const renderer = new marked.Renderer();
+
+// renderer.paragraph = function (text) {
+//   return text.replace(/\r?\n/g, '<br>')
+// };
+// renderer.paragraph = function (text) {
+//   return '<p>' & text & '</p>'
+// };
+
   const markedTextarea = () => {
-    return {__html: marked(previewValue)}
+    return {__html: marked(previewValue,{breaks:true,gfm:true})}
   }
 
   return (
